5462917faa44a961b6e274a013acb40164cb2573
Joha <jotz@outlook.fr>
2023-12-19 16:12:25 +0100

    Added basic JavaScript form handling and integrated typeswriter plug-in
    - basic form handling JS
    - integration of Typewriter plug-in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Recipe will appear hereee",
+    autoStart: true,
+    DelayNode: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
